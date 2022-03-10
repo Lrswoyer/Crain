d57984c396c56513bc4acbd89b76960ef72f7e59
Lrswoyer <98520020+Lrswoyer@users.noreply.github.com>
2022-03-10 14:21:15 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-UUID=237fad57-6a0f-4b6f-95dd-9054fadc7a96
-WSPATH=/kyfey
+UUID=c4314cff-b86a-4981-ba78-3af3b9390268
+WSPATH=/ponu
 PORT=443
 
 # Write V2Ray configuration
