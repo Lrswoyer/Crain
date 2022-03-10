07de99f48af8c1e137578591c0981fcbffc738b9
Lrswoyer <98520020+Lrswoyer@users.noreply.github.com>
2022-03-10 13:57:13 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-UUID=f6dde1a0-e876-4ecf-94c3-5fdeb2d43862
-WSPATH=/vless
-PORT=80
+UUID=8e215dbd-2ce1-4f1e-bad3-89957a0fc9bf
+WSPATH=/ohg
+PORT=39772
 
 # Write V2Ray configuration
 cat << EOF > ${DIR_TMP}/heroku.json
